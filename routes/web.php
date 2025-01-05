e26d8836d09fb24c05597c5d9df3dd943d52e4a6
oufkirhamza <oufkirhamza08@gmail.com>
2025-01-05 19:27:11 +0100

    add product ui && colors

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/addproduct', function () {
+    return view('product.add_product');
+})->middleware(['auth', 'verified'])->name('addproduct');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
+
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -14,6 +15,13 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors:{
+                "alpha" : "#B19D7B",
+                "beta" : "#D7D5CD",
+                "gamma" : "#7E4F02",
+                "delta" : "#CFC4AF",
+                "teta" : "#E9E8E4"
+            }
         },
     },
 
